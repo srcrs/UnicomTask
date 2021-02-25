7a28492501302c64789fb0863d1e867430da2a68
Ningzetao <74031268+Ningzetao@users.noreply.github.com>
2021-02-25 17:10:20 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -12,11 +12,11 @@ def sendEmail():
     #163邮箱服务器地址
     mail_host = 'smtp.163.com'  
     #163用户名
-    mail_user = 'bot2021' 
+    mail_user = '苏苏' 
     #密码(部分邮箱为授权码) 
-    mail_pass = 'KXEXSFCZWFEIASLE'   
+    mail_pass = '1314520.'   
     #邮件发送方邮箱地址
-    sender = 'bot2021@163.com'  
+    sender = 'n2918525416@163.com'  
     #邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
     receivers = [os.environ.get('EMAIL_COVER')]  
     
