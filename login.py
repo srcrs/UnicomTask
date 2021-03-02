ae45b057ff4f8ed2e307deb189a71637f9c6d032
zxwljs <banznx@qq.com>
2021-03-02 15:30:22 +0800

    Update login.py
    去除个人敏感信息

--- login.py
+++ login.py
@@ -121,11 +121,11 @@ def login(username,password,appId):
     try:
         result = response.json()
         if result['code'] == '0':
-            logger.info('【登录】: ' + result['default'][-4:])
+            logger.info('【登录成功】')
             session.headers.update({'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 unicom{version:iphone_c@8.0100}{systemVersion:dis}{yw_code:}'})
             flag = True
         else:
-            logger.info('【登录】: ' + result['dsc'])
+            logger.info('【登录失败】' )
     except Exception as e:
         print(traceback.format_exc())
         logger.error('【登录】: 发生错误，原因为: ' + str(e))
