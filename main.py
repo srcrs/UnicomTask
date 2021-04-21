37e5a84b9aaf64c00b99315f30d38f9ecf95f36d
maodou12138 <31993389+maodou12138@users.noreply.github.com>
2021-04-21 19:47:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -440,6 +440,8 @@ def main(event, context):
             notify.sendIFTTT(user['IFTTT'])
         if('Bark' in user) :
             notify.sendBark(user['Bark'])
+        if('serverchanid' in user)
+            notify.serverchan(user['serverchanid'])
 
 #主函数入口
 if __name__ == '__main__':
