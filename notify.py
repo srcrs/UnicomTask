2f018c1a8b7da1355159a46523c4288aad59aacf
Max丶毛豆 <31993389+maxmaodou@users.noreply.github.com>
2021-04-22 23:55:05 +0800

    Update notify.py

--- notify.py
+++ notify.py
@@ -182,6 +182,6 @@ def sendBark(Bark):
     
 def ServerChanPush(ServerChanSendkey):
     content=readFile_text('./log.txt')
-    subject = user['username']+'UnicomTask每日报表'
+    subject = 'UnicomTask每日报表'
     param= '?title=' +subject + '&desp=' + content
     requests.get('https://sctapi.ftqq.com/'+ServerChanSendkey+'.send' + param)
